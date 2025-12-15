c1a2a72975fc56c29197d3824b1eab4197826b64
Ahmed Gomaa <ag8496001@gmail.com>
2025-12-15 18:36:48 +0200

    Remove duplicate legacy component directories (auth and dashboard)

--- components/dashboard/index.ts
+++
@@ -1,7 +0,0 @@
-// Dashboard components
-export { DashboardLayout } from './DashboardLayout';
-export { DashboardLoading } from './DashboardLoading';
-export { DashboardOverview } from './DashboardOverview';
-export { DashboardStats } from './DashboardStats';
-export { RecentActivity } from './RecentActivity';
-export { QuickActions } from './QuickActions';
