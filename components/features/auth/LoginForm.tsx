d0fa476ce07238d063c6e63b61fc45845c5a84cf
Ahmed Gomaa <ag8496001@gmail.com>
2025-12-15 18:52:01 +0200

    removed legacy code

--- components/features/auth/LoginForm.tsx
+++ components/features/auth/LoginForm.tsx
@@ -8,7 +8,6 @@ import { z } from "zod";
 import { Loader2 } from "lucide-react";
 import Link from "next/link";
 import { toast } from "sonner";
-
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
